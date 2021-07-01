ab44c2d28a1907aadf3d5e638be7e1f175615033
Sidinei Simeão <sidneisimmon@gmail.com>
2021-07-01 15:42:45 -0300

    finalizando parte 1 do curso

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,29 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-import { Title, Form, Repos } from './styles';
+import { api } from '../../Services/api';
+
+import { Title, Form, Repos, Error } from './styles';
 import logo from '../../assets/logo.svg';
 
-export const Dashboard: React.FC = () => {
+interface GithubRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [repos, setRepos] = useState<GithubRepository[]>(() => {
+    const storageRepos = localStorage.getItem('@GitCollection:repositories');
+
+    if (storageRepos) {
+      return JSON.parse(storageRepos);
+    }
+    return [];
+  });
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const formEl = useRef<HTMLFormElement | null>(null);
+
+  useEffect(() => {
+    localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
+  }, [repos]);
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
+    setNewRepo(event.target.value);
+  };
+
+  const handleAddRepo = async (
+    event: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> => {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Informe o username/repositório');
+      return;
+    }
+    try {
+      const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+
+      setRepos([...repos, response.data]);
+
+      setNewRepo('');
+    } catch {
+      setInputError('Repositório não encontrado.');
+    }
+
+    formEl.current?.reset();
+  };
+
   return (
     <>
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de Repositórios do Git</Title>
-      <Form>
-        <input type="text" placeholder="username/repository_name" />
+      <Form
+        ref={formEl}
+        hasError={Boolean(inputError)}
+        onSubmit={handleAddRepo}
+      >
+        <input
+          onChange={handleInputChange}
+          type="text"
+          placeholder="username/repository_name"
+        />
         <button type="submit">Buscar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repos>
-        <a href="/repositories">
-          <img src="https://styled-components.com/logo.png" alt="Repositório" />
-          <div>
-            <strong>auiziodeveloper/mini-curso-reactjs</strong>
-            <p>Repositorio do mini curso gratuito de reactjs</p>
-          </div>
-          <FiChevronRight size="20" />
-        </a>
+        {repos.map((repository, i) => (
+          <Link
+            key={repository.full_name + i}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size="20" />
+          </Link>
+        ))}
       </Repos>
     </>
   );
 };
+
+export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Dashboard } from '../pages/Dashboard';
-import { Repo } from '../pages/Repo';
+const Dashboard = lazy(
+  () =>
+    import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: 'dashboard' */
+      '../pages/Dashboard'
+    ),
+);
+const Repo = lazy(
+  () =>
+    import(
+      /* webpackPrefetch: true */ /* webpackChunkName: 'repo' */ '../pages/Repo'
+    ),
+);
 
 export const Routes: React.FC = () => {
   return (
-    <Switch>
-      <Route component={Dashboard} path="/" exact />
-      <Route component={Repo} path="/repositories" />
-    </Switch>
+    <Suspense fallback={'Aplicação carregando'}>
+      <Switch>
+        <Route component={Dashboard} path="/" exact />
+        <Route component={Repo} path="/repositories/:repository+" />
+      </Switch>
+    </Suspense>
   );
 };
